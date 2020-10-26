81bf62f9e78e29786095286917fc96e7f3ef8113
Pudderz <mpudney6@gmail.com>
2020-10-26 23:04:18 +0000

    Changed recentPosts
    Recent Post does not effect the feature component anymore and just displays a responsive list of posts

--- src/Components/recentPosts.js
+++ src/Components/recentPosts.js
@@ -1,33 +1,21 @@
 import React from 'react'
 import Image from 'gatsby-image'
 import { Link } from 'gatsby';
+import {Grid} from '@material-ui/core'
+import Blog from './blog'
 export const PopularPosts =(props)=> {
-    const onChange=(e)=>{
-        props.onPostClick(e.currentTarget.getAttribute('data-key')); 
-    }
-    
+    const articles = props.data;
     return (
         <div className="popularPosts">
             <h3>Recent Posts</h3>
             <hr/>
-            <ul id="popular">
-                {props.data.allContentfulTeam.edges.map((item, index)=>(
-                    <li key={`${index}`} onClick={onChange} onKeyDown={onChange} data-key={`${index}`}>
-                        <Image
-                        fluid={item.node.featuredImage.fluid}
-                        alt={item.node.post.childMdx.frontmatter.title}
-                        className="image recentPostImage"
-                        style={{'objectFit':'cover', width: '100%', height:'100%',}}
-                        data-key={`${index}`}
-                        />
-                        <div className="middle">
-                            <div className="text">
-                                <p>{item.node.post.childMdx.frontmatter.postedAt}</p>
-                            </div>
-                        </div>
-                    </li>
-                ))}
-            </ul>
+            <ul className="allPosts recent">
+            <Grid container spacing={2}>
+            {articles.map((article, index) => (
+                <Blog key={index} data={article} smallestSize={6}/>
+            ))}
+            </Grid>
+             </ul>
             <hr/>
             <div className="center"><Link to="/posts">All Posts</Link></div>
         </div>
