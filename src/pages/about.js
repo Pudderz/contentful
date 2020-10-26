62d13ae34d84b15d532e0c1a29b5debcf9679f9d
Pudderz <mpudney6@gmail.com>
2020-10-26 22:56:46 +0000

    removed about list
    removed to prepare for a proper detailed about section and added netlify attribute to contact form to make it actually working

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,64 +13,14 @@ class about extends Component {
                 <div id="aboutSite">
                     <h2>About Site</h2>     
                     <p>This is a Gatsby Blog Template site that takes data from a mdx files using graphql and displays the content in a blog format</p>
-                    <p>The plugins I used in this project are:</p>
-                    <ol>
-                        <li>
-                            <p>gatsby-source-filesystem</p>
-                        </li>
-                        <li>
-                            gatsby-plugin-manifest
-                        </li>
-                        <li>
-                            <p>gatsby-transformer-sharp</p>
-                        </li>
-                        <li>
-                            <p>gatsby-plugin-sharp</p>
-                        </li>
-                        <li>
-                           <p>gatsby-awesome-pagination</p> 
-                        </li>
-                        <li>
-                            <p>gatsby-plugin-offline</p>
-                        </li>
-                        <li>
-                            <p>gatsby-plugin-react-helmet</p>
-                        </li>
-                        <li>
-                            <p>gatsby-transformer-sharp</p>
-                        </li>
-                        <li>
-                            <p>gatsby-plugin-sharp</p>
-                        </li>
-                        <li>
-                            <p>gatsby-plugin-sass</p>
-                        </li>
-                        <li>
-                            <p>gatsby-plugin-mdx</p>
-                        </li>
-                        <li>
-                            <p>gatsby-remark-images</p>
-                        </li>
-                        <li>
-                            <p>gatsby-plugin-mdx</p>
-                        </li>
-                        <li>
-                            <p>gatsby-remark-autolink-headers</p>
-                        </li>
-                        <li>
-                            <p>gatsby-remark-slug</p>
-                        </li>
-                        <li>
-                            <p>gatsby-plugin-page-creator</p>
-                        </li>
-                    </ol>
+                    
                 </div>
                 
   
   
 
                 <div id="formDiv">
-                    <form className="contactForm">
+                    <form className="contactForm" netlify>
                          <h3>Contact Form</h3>
                         <label htmlFor="name">
                             Name:<input id="name"type="text" placeholder="name"/>
@@ -79,7 +29,7 @@ class about extends Component {
                             Email:<input id="email"type="email" placeholder="email"/>
                         </label>
                         <label htmlFor="message">
-                            Message:<textarea id="message" type="text" placeholder="message"/>
+                            Message:<textarea id="message" placeholder="message"/>
                         </label>
                         
                         <button className="purple" type="submit">Send</button>
