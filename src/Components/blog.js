6ca99c722c12377d597687cfbb3749d9ee9c4179
Pudderz <mpudney6@gmail.com>
2020-10-26 23:08:42 +0000

    Started using material ui Grid to simplify code

--- src/Components/blog.js
+++ src/Components/blog.js
@@ -1,26 +1,35 @@
 import React from 'react'
 import Image from 'gatsby-image'
 import {Link} from 'gatsby'
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+function Blog({data, smallestSize}) {
 
-function Blog({data}) {
     return (
-        <li key={data.node.post.childMdx.frontmatter.slug} className="blogPost">
-            <div >
+        <Grid className="items" item xs={12} sm={12} md={6} lg={smallestSize} key={data.node.post.childMdx.frontmatter.slug}>
+        {/* <li   */}
+         {/* className="blogPost" */}
+        {/* > */}
+            <div id="imageContainer">
                 <Link to={`/blogs/${data.node.post.childMdx.frontmatter.slug}`}>
                 <Image
                 fluid={data.node.featuredImage.fluid}
                 alt={data.node.post.childMdx.frontmatter.title}
                 className="image"
+                height="300px"
+                width="100%"
                 /></Link>
             </div>
             <div className="information">
                 <h2>{data.node.post.childMdx.frontmatter.title}</h2>
-                <time>Posted At: {data.node.post.childMdx.frontmatter.postedAt}</time>
+                
                 <div dangerouslySetInnerHTML={{__html:data.node.post.childMdx.frontmatter.snippet}}></div>
+                <time>Posted At: {data.node.post.childMdx.frontmatter.postedAt}</time>
                 <Link to={`/blogs/${data.node.post.childMdx.frontmatter.slug}`} className="purple">Read More</Link>   
             </div>
             
-        </li>
+        {/* </li> */}
+        </Grid>
     )
 }
 
--- src/templates/listOfPages.js
+++ src/templates/listOfPages.js
@@ -7,20 +7,25 @@ import Pager from "../Components/pager";
 import Navigation from "../Components/navigation";
 import GoToTopBot from "../Components/goToTopBot";
 import Metadata from "../Components/metadata";
-
+import Grid from '@material-ui/core/Grid';
+import MenuListComposition from '../Components/top'
 const BlogPost = ({ data, pageContext }) => {
   const articles = data.allContentfulTeam.edges;
   return (
     <div>
       <Metadata />
+      <MenuListComposition/>
       <Navigation />
       <h3 style={{ margin: "auto", textAlign: "center" }}>Posts</h3>
       <Pager pageContext={pageContext} />
       <div>
+        
         <ul className="allPosts">
+          <Grid container spacing={2}>
           {articles.map((article, index) => (
-            <Blog key={index} data={article} />
+            <Blog key={index} data={article}  smallestSize={4}/>
           ))}
+          </Grid>
         </ul>
       </div>
       <Pager pageContext={pageContext} />
