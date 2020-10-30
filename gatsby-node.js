a0e17b03bf4a603a4a4be9c9de25750db7936a60
Pudderz <mpudney6@gmail.com>
2020-10-30 16:56:21 +0000

    Added next and previous to pageContext to be able to link between blog posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -30,24 +30,27 @@ exports.createPages = async ({ actions, graphql }) => {
     console.log("There was an error");
     return;
   }
+  const blogPosts= results.data.allContentfulTeam.edges
   //paginates /posts to divide the full list of pages up
   //itemsPerPage edit to change number of items per post page
   paginate({
     createPage,
-    items: results.data.allContentfulTeam.edges,
+    items: blogPosts,
     itemsPerPage: 4,
     pathPrefix: "/posts",
     component: path.resolve("./src/templates/listOfPages.js"),
   });
 
   //creates pages for each blog post
-  results.data.allContentfulTeam.edges.forEach((edge) => {
+  blogPosts.forEach((edge, index) => {
     const product = edge.node.post.childMdx;
     createPage({
       path: `/blogs/${product.frontmatter.slug}`,
       component: require.resolve("./src/templates/blogPage.js"),
       context: {
         slug: product.frontmatter.slug,
+        previous:(index === blogPosts.length-1)? null : blogPosts[index + 1].node.post.childMdx.frontmatter.slug,
+        next: (index === 0)?  null : blogPosts[index-1].node.post.childMdx.frontmatter.slug,
       },
     });
   });
@@ -57,7 +60,7 @@ exports.createPages = async ({ actions, graphql }) => {
     component: path.resolve(`./src/templates/searchTemplate.js`),
     context: {
       blogData: {
-        allBlogs: results.data.allContentfulTeam.edges,
+        allBlogs: blogPosts,
         options: {
           indexStrategy: "All",
           searchSanitizer: "Lower Case",
