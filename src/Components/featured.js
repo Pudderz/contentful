b2076178e5aae17693c6540d185182d4fbf1e0f7
Pudderz <mpudney6@gmail.com>
2020-11-01 11:40:58 +0000

    featured image selection now sticks to the bottom

--- src/Components/featured.js
+++ src/Components/featured.js
@@ -24,39 +24,35 @@ const Featured = (props) => {
   //     props.onPostClick();
   // }
   return (
-    <div
-      className="featuredContainer"
-    >
+    <div className="featuredContainer">
       <div
       // className="featuredImage"
       >
-        <div
+        {/* <div
           style={{
             height: "100%",
             width: "100%",
-            maxHeight: "100%",
-            maxWidth: "100%",
           }}
-        >
+        > */}
           {/* <a
             to={`/blogs/${
               allData.allContentfulTeam.edges[state.featuredIndex].node.post
                 .childMdx.frontmatter.slug
             }`}
           > */}
-            {allData.allContentfulTeam.edges.map((data, index) => (
-              <Image
-                key={index}
-                style={{
-                  height: "100%",
-                }}
-                fluid={data.node.featuredImage.fluid}
-                alt={data.node.post.childMdx.frontmatter.title}
-                className={`image featuredImages ${active[index]} active${index}`}
-                height="300px"
-              />
-            ))}
-            {/* <Image
+          {allData.allContentfulTeam.edges.map((data, index) => (
+            <Image
+              key={index}
+              style={{
+                height: "100%",
+              }}
+              fluid={data.node.featuredImage.fluid}
+              alt={data.node.post.childMdx.frontmatter.title}
+              className={`image featuredImages ${active[index]} active${index}`}
+              height="300px"
+            />
+          ))}
+          {/* <Image
                 style={{
                     height:'100%',
                 }}
@@ -66,95 +62,97 @@ const Featured = (props) => {
                 height="300px"
                 />     */}
           {/* </a> */}
-        </div>
+        {/* </div> */}
       </div>
-      <div className="featuredText">
-        <div>
-          <a
-            style={{width:'fit-content'}}
-            onClick={() =>
-              navigate("/search", {
-                state: {
-                  category:
-                    allData.allContentfulTeam.edges[state.featuredIndex].node
-                      .categories[0],
-                },
-              })
-            }
-            className="category"
-          >
-            {
-              allData.allContentfulTeam.edges[state.featuredIndex].node
-                .categories[0]
-            }
-          </a>
-          <Link
-            to={`/blogs/${
-              allData.allContentfulTeam.edges[state.featuredIndex].node.post
-                .childMdx.frontmatter.slug
-            }`}
-          >
-          <h2 id="featuredTitle" className="underline">
-            {
-              allData.allContentfulTeam.edges[state.featuredIndex].node.post
-                .childMdx.frontmatter.title
-            }
-          </h2></Link>
-
-          <time>
-            Posted At:{" "}
-            {
-              allData.allContentfulTeam.edges[state.featuredIndex].node.post
-                .childMdx.frontmatter.postedAt
-            }
-          </time>
-          {/* <div dangerouslySetInnerHTML={{__html : data.node.post.childMdx.frontmatter.snippet}}/> */}
-          <Link
-            to={`/blogs/${
-              allData.allContentfulTeam.edges[state.featuredIndex].node.post
-                .childMdx.frontmatter.slug
-            }`}
-            className="purple"
-          >
-            Go To Post
-          </Link>
-        </div>
-      </div>
-      <div
-        className="popularSelection"
-        style={{
-          display: "flex",
-          position: "absolute",
-          zIndex: "2",
-          width: "100%",
-          justifyContent: "space-around",
-          bottom: "0",
-        }}
-      >
-        <ul id="popular" className="flexbottom">
-          {allData.allContentfulTeam.edges.map((item, index) => (
-            <li
-              key={`${index}`}
-              onClick={onChange}
-              onKeyDown={onChange}
-              data-key={`${index}`}
+      <div id="textContainer">
+        <div className="featuredText">
+          <div>
+            <a
+              style={{ width: "fit-content" }}
+              onClick={() =>
+                navigate("/search", {
+                  state: {
+                    category:
+                      allData.allContentfulTeam.edges[state.featuredIndex].node
+                        .categories[0],
+                  },
+                })
+              }
+              className="category"
             >
-              <Image
-                fluid={item.node.featuredImage.fluid}
-                alt={item.node.post.childMdx.frontmatter.title}
-                className="image recentPostImage"
-                style={{ margin: "10px" }}
+              {
+                allData.allContentfulTeam.edges[state.featuredIndex].node
+                  .categories[0]
+              }
+            </a>
+            <Link
+              to={`/blogs/${
+                allData.allContentfulTeam.edges[state.featuredIndex].node.post
+                  .childMdx.frontmatter.slug
+              }`}
+            >
+              <h2 id="featuredTitle" className="underline">
+                {
+                  allData.allContentfulTeam.edges[state.featuredIndex].node.post
+                    .childMdx.frontmatter.title
+                }
+              </h2>
+            </Link>
+
+            <time>
+              Posted At:{" "}
+              {
+                allData.allContentfulTeam.edges[state.featuredIndex].node.post
+                  .childMdx.frontmatter.postedAt
+              }
+            </time>
+            {/* <div dangerouslySetInnerHTML={{__html : data.node.post.childMdx.frontmatter.snippet}}/> */}
+            <Link
+              to={`/blogs/${
+                allData.allContentfulTeam.edges[state.featuredIndex].node.post
+                  .childMdx.frontmatter.slug
+              }`}
+              className="purple"
+            >
+              Go To Post
+            </Link>
+          </div>
+        </div>
+        <div
+          className="popularSelection"
+          style={{
+            display: "flex",
+            position: "absolute",
+            zIndex: "2",
+            width: "100%",
+            justifyContent: "space-around",
+            bottom: "0",
+          }}
+        >
+          <ul id="popular" className="flexbottom">
+            {allData.allContentfulTeam.edges.map((item, index) => (
+              <li
+                key={`${index}`}
+                onClick={onChange}
+                onKeyDown={onChange}
                 data-key={`${index}`}
-              />
-              <div className="middle">
-                <div className="text">
-                  <p>{item.node.post.childMdx.frontmatter.title}</p>
+              >
+                <Image
+                  fluid={item.node.featuredImage.fluid}
+                  alt={item.node.post.childMdx.frontmatter.title}
+                  className="image recentPostImage"
+                  style={{ margin: "10px" }}
+                  data-key={`${index}`}
+                />
+                <div className="middle">
+                  <div className="text">
+                    <p>{item.node.post.childMdx.frontmatter.title}</p>
+                  </div>
                 </div>
-              </div>
-            </li>
-          ))}
-        </ul>
-        {/* <IconButton>
+              </li>
+            ))}
+          </ul>
+          {/* <IconButton>
                     <ImageIcon/>
                 </IconButton>
                 <IconButton>
@@ -166,6 +164,7 @@ const Featured = (props) => {
                 <IconButton>
                     <ImageIcon/>
                 </IconButton> */}
+        </div>
       </div>
     </div>
   );
