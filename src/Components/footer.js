06a1eeda27b1dab50f098e77cbb264251a1911fd
Pudderz <mpudney6@gmail.com>
2021-01-04 14:13:59 +0000

    updated footer, removed sub to email list box as i will not use it

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -3,60 +3,18 @@ import { Link } from "gatsby";
 import React, { Component } from "react";
 import CategorySelection from "./categorySelection";
 import './footer.scss'
+
+
 class Footer extends Component {
+
   changeCategory=(category)=>{
     this.props.changeCategory(category)
   }
+  
   render() {
     return (
       <div className="colorPurple">
         <footer className="grid">
-          <div
-            style={{
-              maxWidth: "1300px",
-              margin: "auto",
-              backgroundColor: "#fcbc3e",
-              padding: "30px",
-              color: "black",
-              display: "flex",
-              justifyContent: "space-around",
-              flexWrap: "wrap",
-              width: "100%",
-              boxSizing: "border-box",
-            }}
-          >
-            <h2 style={{ margin: "auto", textAlign: "center" }}>
-              Subscribe to the mailing list
-            </h2>
-            <form style={{ minWidth: "40%" }} netlify name="email" id="outlined-form">
-              <TextField 
-                id="outlined-basic"
-                label="email"
-                variant="outlined"
-                type="email"
-                name="subscribe-email"
-                style={{
-                  backgroundColor: "white",
-                  borderRadius: "0",
-                  
-                }}
-              />
-              <Button
-              type="submit"
-              id="footerSubscribe"
-                style={{
-                  color: "white",
-                  backgroundColor: "#191c1d",
-                  borderRadius: "0",
-                  
-                  // width: '100%',
-                  // margin: '20px 0 0'
-                }}
-              >
-                SubScribe
-              </Button>
-            </form>
-          </div>
           <hr
             style={{ width: "100%", maxWidth: "1300px", margin: "24px auto 0" }}
           />
@@ -77,11 +35,10 @@ class Footer extends Component {
               flexWrap: 'wrap',
             }}
           >
-            <Link to="/about">About us</Link>
-            <Link to="/">Contact</Link>
-            <Link to="/">All Articles</Link>
-            <Link to="/">Privacy policy</Link>
-            <Link to="/">Cookies</Link>
+            <Link to="/">Home</Link>
+            <Link to="/about">About </Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/posts">All Articles</Link>
           </div>
         </footer>
       </div>
