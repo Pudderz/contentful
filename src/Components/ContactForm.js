8c17e1a56f3d7622aae893d0203bf3d16a32cc04
Pudderz <mpudney6@gmail.com>
2021-01-04 13:53:45 +0000

    added contact oage

---
+++ src/Components/ContactForm.js
@@ -0,0 +1,192 @@
+import { Button } from "@material-ui/core";
+import React, { useState } from "react";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { makeStyles } from "@material-ui/core/styles";
+import { green } from "@material-ui/core/colors";
+
+
+const encode = (data) => {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '50%',
+    padding: '20px',
+    boxSizing: 'border-box',
+    '& button':{
+      margin: '20px 0',
+    }
+  },
+  wrapper: {
+    margin: theme.spacing(1),
+    position: "relative",
+    width: "fit-content",
+    display:'inline-block',
+    margin:'auto',
+  },
+  buttonSuccess: {
+    backgroundColor: green[500],
+    "&:hover": {
+      backgroundColor: green[700],
+    },
+  },
+  buttonProgress: {
+    color: green[500],
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    marginTop: -12,
+    marginLeft: -12,
+    "& svg": {
+      height: "100%",
+      width: "100%",
+      bottom: 0,
+    },
+  },
+  formInputs: {
+    fontFamily: "unset",
+    outline: "none",
+    padding: "1px",
+    fontSize: "16px",
+  },
+  successMessage:{
+    color: green[500],
+    display:'inline-block',
+    width:'fit-content',
+    paddingLeft:'30px',
+  }
+}));
+
+export default function ContactForm() {
+  const classes = useStyles();
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [form, setForm] = useState({
+    formUsername: "",
+    formEmail: "",
+    formMessage: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!loading) {
+      setSuccess(false);
+      setLoading(true);
+
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: encode({ "form-name": "contact", ...form }),
+      })
+        .then(() => {
+          //Adds a delay to the form sending so it is not instant
+          setTimeout(() => {
+            setSuccess(true);
+            setLoading(false);
+            setForm({
+              formUsername: "",
+              formEmail: "",
+              formMessage: "",
+            });
+          }, 500);
+        })
+        .catch((error) => {
+          console.error(error);
+          setLoading(false);
+        });
+    }
+  };
+
+  const handleChange = (e) => {
+    if (!loading) {
+      if(success){
+        setSuccess(false);
+      }
+      setForm({ ...form, [e.target.name]: e.target.value });
+    }
+  };
+
+  return (
+      <div className={`${classes.root} form`}>
+        <h2>Contact Form</h2>
+
+        <hr />
+        {/* form designed to work with for netlify forms */}
+        <form
+          onSubmit={handleSubmit}
+          data-netlify="true"
+          name="contact"
+          style={{ display: "flex", flexDirection: "column" }}
+        >
+          <input type="hidden" name="form-name" value="contact" required />
+          
+          <label htmlFor="name">Name:</label>
+          <input
+            id="name"
+            type="text"
+            name="formUsername"
+            value={form.formUsername}
+            onChange={handleChange}
+            onKeyDown={handleChange}
+            className={classes.formInputs}
+            placeholder="Name"
+            required
+          />
+
+          <label htmlFor="email">Email:</label>
+          <input
+            id="email"
+            type="email"
+            name="formEmail"
+            value={form.formEmail}
+            onChange={handleChange}
+            onKeyDown={handleChange}
+            className={classes.formInputs}
+            placeholder="Email"
+            required
+          />
+
+          <label htmlFor="message">Message:</label>
+          <textarea
+            required
+            id="message"
+            name="formMessage"
+            value={form.formMessage}
+            className={classes.formInputs}
+            onChange={handleChange}
+            onKeyDown={handleChange}
+            style={{
+              resize: "vertical",
+              minHeight: "75px",
+              maxHeight: "500px",
+            }}
+            placeholder="Message"
+          />
+          <div className={classes.wrapper}>
+          
+            <Button
+              variant="contained"
+              color="default"
+              id="submitForm"
+              type="submit"
+              disabled={loading}
+              style={{ width: "100px" }}
+            >
+              Send
+            </Button>
+            {loading && (
+              <CircularProgress size={24} className={classes.buttonProgress} />
+            )}
+          {success && (
+                        <p className={classes.successMessage}>Message sent</p>
+                      )}
+          
+          </div>
+          
+        </form>
+      </div>
+  );
+}
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,50 @@
-import React, { Component } from "react";
+import React from "react";
 import Footer from "../Components/footer";
 import Metadata from "../Components/metadata";
 import "../Components/about.scss";
 import MenuListComposition from "../Components/top";
+import { Link } from "gatsby"
+import { Fab, makeStyles, Tooltip } from "@material-ui/core";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import ContactForm from "../Components/ContactForm";
 
-export const contact = () => {
+const useStyles = makeStyles({
+  contactInfo: {
+    display: "flex",
+    padding: 0,
+  },
+  contact: {
+    width: "50%",
+    padding: "20px",
+    boxSizing: "border-box",
+    "& button": {
+      margin: "20px 0",
+    },
+  },
+  section:{
+    backgroundColor: '#191c1d',
+    color: 'white',
+    overflow: 'hidden',
+    '& h2,h3,h4':{
+      margin:'0',
+    }
+  },
+  header:{
+    display:'flex',
+    gap:'1em',
+    justifyContent:'center',
+    padding:'0',
+    margin:'20px 0',
+    '& h2':{
+      fontSize:'3em',
+      margin:'0',
+    }
+  }
+});
+
+export const Contact = () => {
+  const classes=useStyles();
     return (
         <div style={{ textAlign: "center" }}>
         <MenuListComposition/>
@@ -19,55 +59,129 @@ export const contact = () => {
           }}
         >
        
-
           <div
-            id="formDiv"
-            style={{
-              //   height: '100%',
-              boxSizing: "border-box",
-              backgroundolor: "#242a2c",
-            }}
-          >
-            <form className="contactForm" netlify>
-              <h3>Contact Form</h3>
-              <label htmlFor="name">
-                Name:
-                <input id="name" name="contact-name"type="text" placeholder="name" required/>
-              </label>
-              <label htmlFor="email">
-                Email:
-                <input id="email" name="contact-email"type="email" placeholder="email" required/>
-              </label>
-              <div
+        style={{ minHeight: "100vh", backgroundColor: "#191c1d" }}
+      >
+        <div
+          className="top"
+          style={{
+            zIndex: "400",
+            justifyContent: "flex-end",
+            width: "90%",
+            margin: "7px auto 0",
+          }}
+        >
+     
+
+          <Link to="/">
+            <Tooltip title="Home">
+              <a
                 style={{
-                  position: "relative",
-                  width: "fit-content",
-                  margin: "5px auto 0px",
+                  color: "white",
+                  pointerEvents: "all",
+                  cursor: "pointer",
+                  marginTop: "10px",
                 }}
               >
-                <label
-                  htmlFor="message"
-                  id="messageLabel"
+                Home
+              </a>
+            </Tooltip>
+          </Link>
+        </div>
+
+        <div className={classes.section}>
+          <div>
+            <div className="textContainer" style={{ marginTop: "20px" }}>
+              <header id="flexHeader">
+                <h2>Hey, I'm Matthew</h2>
+
+                <div
                   style={{
-                    margin: "0px",
-                    top: "0%",
-                    left: "23px",
+                    display: "flex",
+                    gap: "1em",
+                    justifyContent: "center",
                   }}
                 >
-                  Message:
-                </label>
-                <textarea id="message" name="contact-message" placeholder="message" style={{}} required/>
-              </div>
+                  <Tooltip title="LinkedIn profile">
+                    <a
+                      href="https://www.linkedin.com/in/matthew-pudney/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Fab size="small">
+                        <LinkedInIcon />
+                      </Fab>
+                    </a>
+                  </Tooltip>
+                  <Tooltip title="Github profile">
+                    <a
+                      href="https://github.com/Pudderz"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Fab size="small">
+                        <GitHubIcon />
+                      </Fab>
+                    </a>
+                  </Tooltip>
+                  
+                </div>
+              </header>
 
-              <button className="purple" type="submit">
-                Send
-              </button>
-            </form>
+              <p style={{ textAlign: "center" }}>
+                I'm a front-end web developer based in UK, looking for a
+                full-time role.
+              </p>
+            </div>
+            <div className="textContainer" style={{ margin: "0 auto" }}>
+              <div>
+                <h2>About Me</h2>
+                <hr />
+                <div></div>
+                <p>
+                  I love learning the massive scope of computer science which I
+                  find incredibly rewarding and exciting as there's always
+                  something more you can learn and improve on.
+                </p>
+                <div style={{ padding: "10px" }}></div>
+                <p>
+                  I'm currently looking for an opportunity to work on a
+                  JavaScript framework and hope to learn and move to a
+                  full-stack position in the future.
+                </p>
+              </div>
+            </div>
           </div>
+        </div>
+
+        <div className={classes.section}>
+          <div
+            className={`textContainer ${classes.contactInfo}`}
+            id="contactInfo"
+            style={{ margin: "0 auto" }}
+          >
+            <div id="contact"
+            className={classes.contact}
+            >
+              <h2>Get in touch</h2>
+
+              <hr />
+              <p>
+                Thank you for checking out my portfolio and if you'd like to
+                learn more about what makes me tick or if you have knowledge of
+                any exciting opportunities feel free to reach out.
+              </p>
+              <p>To get in touch, contact via my email: mpudney2@gmail.com</p>
+              <p>Or by this contact form:</p>
+            </div>
+            <ContactForm />
+          </div>
+        </div>
+      </div>
         </div>
         <Footer />
       </div>
     )
 }
 
-export default contact
+export default Contact
