b32243ad94c6334b6fbf00d2ede68f34a137d03a
Pudderz <mpudney6@gmail.com>
2021-03-03 13:43:23 +0000

    Redesigned homepage
    Commented out featured posts section as I am testing with a simplified design and may add simplified form of it in later.
    
    Added recentPosts, popularPosts and aboutSection to frontPage.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,63 +1,58 @@
 import React, { useState } from "react";
-import Featured from "../Components/featured";
 import "../Components/styles.scss";
 import RecentPosts from "../Components/recentPosts";
 import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import Metadata from "../Components/metadata";
-import MenuListComposition from '../Components/top'
+import MenuListComposition from "../Components/top";
 import Footer from "../Components/footer";
-import '../Components/homePage.scss'
-import { Link } from 'gatsby';
-import { MostPopular } from "../Components/MostPopular";
+import "../Components/homePage.scss";
 import AboutSection from "../Components/AboutSection";
-import AdSense from 'react-adsense';
+import AdSense from "react-adsense";
+import { PopularPosts } from "../Components/PopularPosts";
+import {Featured } from '../Components/featured'
 function Home({ data }) {
- 
-
   return (
     <>
       <Metadata />
-      
-      
-      <div style={{ backgroundColor:'rgb(25, 28, 29)', 'padding': '10px',
-    color: '#fff', textAlign: 'center'}}>
-      <h1 style={{margin:'auto', fontSize:"50px"}}>Blog</h1>
-      
-      </div>
-      <MenuListComposition background="rgb(25, 28, 29)"/>
+
+      <MenuListComposition background="rgb(25, 28, 29)" />
 
       <main>
-        <div style={{margin:'auto', maxWidth:'1300px'}}>
-         <div className="featured" style={{marginTop:'50px'}}>
-          <Featured
+        <div style={{ margin: "auto", maxWidth: "1300px" }}>
+          <div className="featured" style={{ marginTop: "50px" }}>
+            {/* <Featured
             // data={data.allContentfulTeam.edges[state.featuredIndex]}
             allData={data}
             // onPostClick={onPostClick}
           ></Featured>
-        </div>
-        {/* <RecentPosts data={data.allContentfulTeam.edges} />  */}
-        <MostPopular/>
-        <AboutSection/>
-        <hr/>
-            <div className="center"><Link to="/posts">All Posts</Link>
-            
-            <Link to="/posts">Search</Link>
-            <Link to="/posts">About</Link>
+           */}
+          
+          </div>
+          <div
+          className="frontPageContainer"
+          
+          >
+            <RecentPosts data={data.allContentfulTeam.edges} />
+            <PopularPosts data={data.allContentfulTeam.edges} />
+
+            <div
+            className="clampContainer"
+            >
+              <AboutSection />
             </div>
-            <AdSense.Google
+          </div>
+
+          {/* <AdSense.Google
               client='ca-pub-1286129126098643'
-              slot='7806394673'
+              // slot='7806394673'
               // style={{ display: 'block' }}
               // layout='in-article'
               // format='fluid'
-            />
- 
+            /> */}
         </div>
-               
-        
       </main>
-      <Footer/>
+      <Footer />
     </>
   );
 }
@@ -75,7 +70,7 @@ export const query = graphql`
         node {
           categories
           featuredImage {
-            fluid(maxWidth: 1300,quality: 90) {
+            fluid(maxWidth: 1300, quality: 90) {
               ...GatsbyContentfulFluid
             }
           }
