5d6b4552d48b03ee18c76f8662793f11888ecf4b
Pudderz <mpudney6@gmail.com>
2021-03-03 13:36:06 +0000

    Changed Recent Posts and added PopularBlog Component to it
    Changed Recent Posts by simplifying the design (removed images and snippets in the recent blog lists)

---
+++ src/Components/PopularBlog.js
@@ -0,0 +1,78 @@
+import React from "react";
+import Image from "gatsby-image";
+import { Link } from "gatsby";
+
+import li from "@material-ui/core/Grid";
+function PopularBlog({ data }) {
+  console.log(data);
+  return (
+    <li
+      key={data.node.post.childMdx.frontmatter.slug}
+      style={{
+        display: "flex",
+        flexFlow: "column",
+        padding: "0px",
+        flexGrow: "2",
+        justifyContent: "space-between",
+        boxSizing: "border-box",
+      }}
+    >
+      <div style={{ display: "flex" }}>
+        <div className="items" style={{ width: "100%" }}>
+          {/* <p style={{ padding: "0 20px" }}>{index + 1}</p>   */}
+          {/* <hr/> */}
+          <Link to={`/blogs/${data.node.post.childMdx.frontmatter.slug}`}>
+            <div
+              className="information"
+              // style={{width:'80%'}}
+            >
+              <h2
+                style={{
+                  fontSize: "20px",
+                  fontFamily: "system-ui",
+                  display: "-webkit-box",
+                  WebkitLineClamp: "2",
+                  WebkitBoxOrient: "vertical",
+                  // height: "54px",
+                  margin: "0",
+                }}
+              >
+                {data.node.post.childMdx.frontmatter.title}
+              </h2>
+              <ul
+                style={{
+                  padding: "0",
+                  display: "flex",
+                  gap: "10px",
+                  listStyle: "none",
+                }}
+              >
+                {data.node.categories.map((category, index) => (
+                  <li
+                    key={category}
+                    style={{
+                      borderRadius: "5px",
+                      backgroundColor: "rgba(0,200,0,0.2)",
+                      padding: "1px 5px",
+                      width: "fit-content",
+                      margin: "5px 0 ",
+                    }}
+                  >
+                    {category}
+                  </li>
+                ))}
+              </ul>
+
+              <time style={{ color: "rgb(42, 48, 57)", fontSize: "14px" }}>
+                {data.node.post.childMdx.frontmatter.postedAt}
+              </time>
+            </div>
+          </Link>
+        </div>
+      </div>
+      {/* </li> */}
+    </li>
+  );
+}
+
+export default PopularBlog;
--- src/Components/recentPosts.js
+++ src/Components/recentPosts.js
@@ -1,25 +1,57 @@
-import React from 'react'
-import { Link } from 'gatsby';
-import {Grid} from '@material-ui/core'
-import Blog from './blog'
-export const PopularPosts = props => {
-    const articles = props.data;
-    return (
-        <div className="popularPosts"style={{padding:'20px', boxSizing:'border-box'}}>
-            <h3>Recent Posts</h3>
-            <hr/>
-            
-            <Grid container  className="allPosts recent">
-            {articles.map((article, index) => (
-                <Blog key={index} data={article} smallestSize={6} small={12}/>
-            ))}
-            </Grid>
-          
-            <hr/>
-            <div className="center"><Link to="/posts">All Posts</Link></div>
-        </div>
-    )
-    
-}
+import React from "react";
+import { Link } from "gatsby";
+import { Grid } from "@material-ui/core";
+import Blog from "./blog";
+import { Breadcrumbs, Typography } from "@material-ui/core";
+import PopularBlog from "./PopularBlog";
+export const RecentPosts = (props) => {
+  const articles = props.data;
+  return (
+    <div
+      className="popularPosts"
+      style={{
+        minWidth: "min(300px,33%)",
+        boxSizing: "border-box",
+        minWidth: "300px",
+        maxWidth: "1000px",
+        margin: "0 auto",
+        flexGrow:"2",
+      }}
+    >
+      <h3 style={{ fontSize: "1.5em", color: "#4a5568" }}>Recent Posts</h3>
 
-export default PopularPosts
+      <ul
+        style={{
+          gap: "20px",
+          display: "grid",
+          padding: "0 10px",
+          boxSizing: "border-box",
+        }}
+      >
+        {articles.map((article, index) => (
+          <PopularBlog
+            key={index}
+            data={article}
+            smallestSize={6}
+            small={6}
+            index={index}
+          />
+        ))}
+        <li
+          style={{
+            display: "flex",
+            flexFlow: "column",
+            padding: "0px",
+
+            justifyContent: "space-between",
+            boxSizing: "border-box",
+          }}
+        >
+          <Link to="/posts">View More...</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default RecentPosts;
